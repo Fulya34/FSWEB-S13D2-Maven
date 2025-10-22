f5d0a3eaffa1591b9ee024387526abfdc70ee441
Fulya34 <funda.sisek_49@icloud.com>
2025-10-22 21:36:10 +0300

    first commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,45 @@
 package org.example;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    public static boolean isPalindrome(int sayi) {
+        String[] chars = Integer.toString(Math.abs(sayi)).split("");
+        String reversed = "";
+        for (int i = chars.length - 1; i >= 0; i--) {
+            reversed += chars[i];
+        }
+        return Integer.parseInt(reversed) == Math.abs(sayi);
+    }
+
+    public static boolean isPerfectNumber(int number) {
+        if (number < 0)
+            return false;
+
+        int toplam = 0;
+        for (int i = 1; i < number; i++) {
+            if (number % i == 0) {
+                toplam += i;
+            }
+        }
+        return number == toplam;
+    }
+
+    public static String numberToWords(int number) {
+        if (number < 0) {
+            return "Invalid Value";
+        }
+
+        String[] words = {"Zero", "One", "Two", "Three", "Four",
+                "Five", "Six", "Seven", "Eight", "Nine"};
+
+        String[] digits = String.valueOf(number).split("");
+        StringBuilder result = new StringBuilder();
+
+        for (String d : digits) {
+            int rakam = Integer.parseInt(d);
+            result.append(words[rakam]).append(" ");
+        }
+
+        return result.toString().trim();
     }
 }
